38dd3f1c14ebecf9a47f77715e4bffe4281ffdad
gmekonnen <getachew.mekonnen2010@gmail.com>
2017-07-02 16:20:39 -0400

    Update PeopleDbContext.cs

--- EFPraSolution/PeopleConsoleApplication/Repo/Entities/PeopleDbContext.cs
+++ EFPraSolution/PeopleConsoleApplication/Repo/Entities/PeopleDbContext.cs
@@ -13,7 +13,53 @@ namespace PeopleConsoleApplication.Repo.Entities
         }
 
         public virtual DbSet<Person> People { get; set; }
+        public virtual int? InsertPerson(PersonDto personEntity)
+        {
+            int? personId = default(int?);
 
+            if (personEntity != null)
+            {
+                var sqlParams = new List<SqlParameter>();
+
+                var personIdOutputParam = new SqlParameter()
+                {
+                    Direction = System.Data.ParameterDirection.Output,
+                    ParameterName = "@personId",
+                    Value = 0,
+                    DbType = System.Data.DbType.Int32
+                };
+
+                sqlParams.Add(personIdOutputParam);
+
+                sqlParams.Add(new SqlParameter()
+                {
+                    ParameterName = "@personName",
+                    Value = personEntity.Name,
+                   
+                });
+
+                sqlParams.Add(new SqlParameter()
+                {
+                    ParameterName = "@registredOn",
+                    Value = personEntity.RegistredOn,
+                   
+                });
+
+               
+                
+                    var commandText = "exec [dbo].[InsertPerson] @personName, @registredOn, @personId OUTPUT";
+                    var result = this.Database.ExecuteSqlCommand(commandText, sqlParams.ToArray());
+
+                    int newPersonId = 0;
+
+                    if (int.TryParse(personIdOutputParam.Value.ToString(), out newPersonId))
+                    {
+                        personId = newPersonId;
+                    }
+                
+            }
+            return personId;
+        }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
         }
