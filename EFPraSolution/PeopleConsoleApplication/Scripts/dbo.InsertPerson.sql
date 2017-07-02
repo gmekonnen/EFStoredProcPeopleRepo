d9cab85b357db8aa07a31dcffddc0b0e21e5f689
meadorable <meadorable@PCADORABLE>
2017-07-02 12:30:04 -0400

    added sql scripts

---
+++ EFPraSolution/PeopleConsoleApplication/Scripts/dbo.InsertPerson.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[InsertPerson]
+	@personName nvarchar(255),
+	@registredOn	datetime,
+	@personId int output
+AS
+	INSERT INTO dbo.People
+	(Name, RegistredOn)
+	VALUES
+	(@personName,
+	@registredOn)
+
+	SELECT @personId = SCOPE_IDENTITY()
+RETURN 0
---
+++ EFPraSolution/PeopleConsoleApplication/Scripts/dbo.People.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[People] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (255) NOT NULL,
+    [RegistredOn] DATETIME       NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
